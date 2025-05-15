188e7224adb987c66a308c1433633db2ed9eb500
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-05-15 16:36:21 +0200

    feat: add pdf input and viewer components

---
+++ src/app/pdf-input/pdf-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pdf-input',
+  imports: [],
+  templateUrl: './pdf-input.component.html',
+  styleUrl: './pdf-input.component.scss'
+})
+export class PdfInputComponent {
+
+}
