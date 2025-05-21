48b71acba2ec262845197061dd5a645bc18c158c
Adrienn Pozsgai <apozsgai97@gmail.com>
2025-05-21 11:06:30 +0200

    feat: refactor PDF input component to improve file selection UI and functionality

--- src/app/pdf-input/pdf-input.component.ts
+++ src/app/pdf-input/pdf-input.component.ts
@@ -1,14 +1,23 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-pdf-input',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './pdf-input.component.html',
   styleUrl: './pdf-input.component.scss',
 })
 export class PdfInputComponent {
-  @Output() fileSelected = new EventEmitter<File>()
-  
+  @Output() fileSelected = new EventEmitter<File>();
+
+  triggerFileInput() {
+    const fileInput = document.getElementById('pdf') as HTMLInputElement;
+    if (fileInput) {
+      fileInput.click();
+    }
+  }
+
   onFileSelected(event: Event) {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
